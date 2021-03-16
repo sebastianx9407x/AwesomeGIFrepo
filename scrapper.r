bbfde59c39de14d3937048c6eae2e5c2e15fec18
Marx <boyuan@vt.edu>
2021-03-16 09:26:23 -0400

    rtweet

--- scrapper.r
+++ scrapper.r
@@ -2,6 +2,15 @@
 #library(rtweet)
 #documentation: https://github.com/ropensci/rtweet
 
-rt <- search_tweets(
-  "#happy", n = 18000, include_rts = FALSE
-)
+#happy, sad, fear, angry, surprise
+rt <- search_tweets('#gif', n=10000, include_rts = FALSE)
+save_as_csv(rt, file_name = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/gif.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+
+#q1 <- matrix(unlist(rt$text))
+#q2 <- matrix(unlist(rt$media_url))
+#df <- cbind(q1, q2)
+#df <- as.data.frame(df)
+#names(df) <- c("text", "media")
+
+#write.csv(x=df, file = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/sample.csv")
+
