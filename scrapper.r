bf4257db4e955c17946b947d5502ca047c1a610e
Marx <boyuan@vt.edu>
2021-03-16 09:27:17 -0400

    some comments

--- scrapper.r
+++ scrapper.r
@@ -2,6 +2,10 @@
 #library(rtweet)
 #documentation: https://github.com/ropensci/rtweet
 
+#limit 18000 per 15 min
+#search all tweets from recent 6-9 days
+#search_tweets will automatically stop if all tweets has been found.
+
 #happy, sad, fear, angry, surprise
 rt <- search_tweets('#gif', n=10000, include_rts = FALSE)
 save_as_csv(rt, file_name = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/gif.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
