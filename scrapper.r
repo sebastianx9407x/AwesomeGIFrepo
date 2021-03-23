c54329a5d11ba402e257a6c57b062de947fd0935
joannafg <joannafg@vt.edu>
2021-03-23 13:12:11 -0400

    frame rate model started

--- scrapper.r
+++ scrapper.r
@@ -7,8 +7,8 @@
 #search_tweets will automatically stop if all tweets has been found.
 
 #happy, sad, fear, angry, surprise
-rt <- search_tweets('#gif', n=10000, include_rts = FALSE)
-save_as_csv(rt, file_name = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/gif.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+#rt <- search_tweets('#gif', n=10000, include_rts = FALSE)
+#save_as_csv(rt, file_name = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/gif.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
 
 #q1 <- matrix(unlist(rt$text))
 #q2 <- matrix(unlist(rt$media_url))
@@ -18,3 +18,11 @@ save_as_csv(rt, file_name = "/Users/marxw/Library/Mobile Documents/com~apple~Clo
 
 #write.csv(x=df, file = "/Users/marxw/Library/Mobile Documents/com~apple~CloudDocs/Desktop/CODE/Python/AwesomeGif/sample.csv")
 
+x1 <- c(2,3,6,8)
+x2 <-c(5,6,7,8)
+y <- c(1,1,1,1)
+
+x <- cbind(x1, x2)
+df <- data.frame(x,y)
+reg <- lm(y~x, data=df)
+summary(reg)
